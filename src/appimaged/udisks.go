31ad9c049dc7622151b6bc9efc07bf18c7fe7dfe
probonopd <probonopd@users.noreply.github.com>
2019-11-03 18:27:46 +0000

    Use org.kde.Solid, too

--- src/appimaged/udisks.go
+++ src/appimaged/udisks.go
@@ -31,6 +31,34 @@ this on other systems using e.g.,
 
 dbus-monitor 2>&1 | grep -i mount
 
+Which on KDE shows... nothing.
+On KDE we get something else which mentions "freedesktop_2FUDisks2" though:
+
+When a volume is mounted:
+signal time=1572805120.557118 sender=:1.34 -> destination=(null destination) serial=227 path=/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1; interface=org.kde.Solid.Device; member=setupRequested
+signal time=1572805120.612607 sender=:1.34 -> destination=(null destination) serial=228 path=/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1; interface=org.kde.Solid.Device; member=setupDone
+
+When a volume is unmounted:
+signal time=1572805151.390507 sender=:1.34 -> destination=(null destination) serial=230 path=/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1; interface=org.kde.Solid.Device; member=teardownRequested
+signal time=1572805151.453440 sender=:1.34 -> destination=(null destination) serial=231 path=/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1; interface=org.kde.Solid.Device; member=teardownDone
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='setupRequested'"
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='teardownRequested'"
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='setupDone'"
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='teardownDone'"
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='setupDone'"
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='teardownRequested'"
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='setupRequested'"
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='teardownDone'"
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='setupRequested'"
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='teardownRequested'"
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='teardownDone'"
+   string "type='signal',path='/org/kde/solid/Device__2Forg_2Ffreedesktop_2FUDisks2_2Fblock_5Fdevices_2Fsdd1',interface='org.kde.Solid.Device',member='setupDone'"
+
+Why such a storm of messages?
+
+So it seems that despite mentions of "freedesktop" and "UDisks2", org.kde.Solid.Device
+is responsible for these kinds of messages on KDE.
+
 Wouldn't it make sense for XDG to standardize MountAdded, MountRemoved
 dbus messages that would be broadcast everyone interested (so that one doesn't
 need to "eavesdrop"), independent of whether the GNOME desktop is used?
@@ -41,9 +69,21 @@ func monitorUdisks(conn *dbus.Conn) {
 
 	// Check whether UDisks2VolumeMonitor is available, exit otherwise
 	var s string
+	bool satisfied := false
 	e := conn.Object("org.gtk.vfs.UDisks2VolumeMonitor", "/").Call("org.freedesktop.DBus.Introspectable.Introspect", 0).Store(&s)
 	if e != nil {
-		log.Println(os.Stderr, "Failed to introspect org.gtk.vfs.UDisks2VolumeMonitor", e)
+		log.Println("Failed to introspect org.gtk.vfs.UDisks2VolumeMonitor", e)
+	} else {
+		satisfied = true
+	}
+	e = conn.Object("org.kde.Solid", "/").Call("org.freedesktop.DBus.Introspectable.Introspect", 0).Store(&s)
+	if e != nil {
+		log.Println("Failed to introspect org.kde.Solid", e)
+	} else {
+		satisfied = true
+	}
+	
+	if satisfied == false {
 		os.Exit(1)
 	}
 
@@ -52,12 +92,13 @@ func monitorUdisks(conn *dbus.Conn) {
 		// "interface='org.gtk.Private.RemoteVolumeMonitor'",
 		"member='MountAdded'",   // org.gtk.Private.RemoteVolumeMonitor
 		"member='MountRemoved'", // org.gtk.Private.RemoteVolumeMonitor
+		"member='setupDone'", // org.kde.Solid.Device
 	}
 	var flag uint = 0
 
 	call := conn.BusObject().Call("org.freedesktop.DBus.Monitoring.BecomeMonitor", 0, rules, flag)
 	if call.Err != nil {
-		log.Println(os.Stderr, "Failed to become monitor:", call.Err)
+		log.Println("Failed to become monitor:", call.Err)
 		return
 	}
 
