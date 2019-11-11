6ab7df1f22dd725b4fa01117b043735e0acdfe23
probonopd <probonopd@users.noreply.github.com>
2019-11-12 00:48:00 +0100

    Debug

--- src/appimaged/prerequisites.go
+++ src/appimaged/prerequisites.go
@@ -147,7 +147,7 @@ func ensureRunningFromLiveSystem() {
 			found = true
 		}
 	}
-	if found == false {
+	if found == false && gcEnvIsThere == false {
 		println("Not running on one of the supported Live systems.")
 		println("Grab a Ubuntu, Debian, Deepin, Fedora, openSUSE, elementary OS, KDE neon,... Live ISO and try from there.")
 		os.Exit(1)
