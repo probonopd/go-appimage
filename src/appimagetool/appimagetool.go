7499cd99696becdbee43521cb9dcc4b6776bb477
probonopd <probonopd@users.noreply.github.com>
2019-11-23 20:33:54 +0100

    Move CalculateSHA256Digest to helpers

--- src/appimagetool/appimagetool.go
+++ src/appimagetool/appimagetool.go
@@ -417,36 +417,7 @@ func GenerateAppImage(appdir string) {
 	// This is actually very useful for PubSub notifications, to find out whether we have already have
 	// an identical AppImage on the local machine or not
 
-	// Calculate AppImage MD5 digest according to
-	// https://github.com/AppImage/libappimage/blob/4d6f5f3d5b6c8c01c39b8ce0364b74cd6e4043c7/src/libappimage_shared/digest.c
-	// The ELF sections
-	// .digest_md5
-	// .sha256_sig
-	// .sig_key
-	// need to be skipped, although I think
-	// .upd_info
-	// ought to be skipped, too
-
-	fmt.Println("Calculating the sha256 digest...")
-	var byteRangesToBeAssumedEmpty []helpers.ByteRange
-	sectionsToBeSkipped := []string{".digest_md5", ".sha256_sig", ".sig_key"}
-	for _, s := range sectionsToBeSkipped {
-		offset, length, err := helpers.GetSectionOffsetAndLength(target, s)
-		if err == nil {
-			fmt.Println("Section", s, "offset", offset, "length", length)
-			br := helpers.ByteRange{int64(offset), int64(length)}
-			byteRangesToBeAssumedEmpty = append(byteRangesToBeAssumedEmpty, br)
-		}
-	}
-
-	f, err := os.Open(target)
-	if err != nil {
-		helpers.PrintError("Cannot open file", err)
-		os.Exit(1)
-	}
-	defer f.Close()
-	h := helpers.CalculateDigestSkippingRanges(f, byteRangesToBeAssumedEmpty)
-	fmt.Printf("Calculated sha256 digest: %x\n", h.Sum(nil))
+	helpers.CalculateSHA256Digest(target, err)
 
 	// TODO: Signing.
 
