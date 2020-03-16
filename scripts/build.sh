b60b8c93aa8ceab75e9693ae82cae8906992510f
probonopd <probonopd@users.noreply.github.com>
2020-03-16 18:20:57 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -47,7 +47,7 @@ if [ $(go env GOHOSTARCH) == "arm64" ] ; then sudo apt-get -y install gcc-arm-li
 # Build appimagetool
 ##############################################################
 
-go get -v github.com/probonopd/go-appimage
+go get -v github.com/probonopd/go-appimage || true
 cd $GOPATH/src
 
 # 64-bit
