probonopd/go-appimage

commit 71d52986d56ab486f97f4e6a998a1d7495b47e3c

Fix arch detection (#81)
* fix: detection of ARCH from libraries

Previously, go-appimagetool did not stop if a proper architecture was
not detected from the .so. files or the binary. As it did not halt,
the arch variable was set to empty string (''). This led to a
filenotfound panic, because the tool was looking for
`release-` instead of `release-x86_64` as the arch variable was
empty. go-appimagetoold should properly halt if the detection of
ARCH variable did not complete successfully, so that they
can provide the ARCH environment variable instead.

* style: run go fmt

src/appimagetool/appimagetool.go
@@ -6,8 +6,11 @@ package main
 import (
 	// "crypto/md5"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"github.com/probonopd/go-zsyncmake/zsync"
+	"gopkg.in/ini.v1"
 	"io/ioutil"
 	"log"
 	"os"
@@ -16,9 +19,6 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
-	"github.com/probonopd/go-zsyncmake/zsync"
-	"gopkg.in/ini.v1"
 
 	"github.com/probonopd/go-appimage/internal/helpers"
 )
@@ -334,13 +334,22 @@ func GenerateAppImage(appdir string) {
 			err := filepath.Walk(appdir, func(path string, info os.FileInfo, err error) error {
 				if err != nil {
 					helpers.PrintError("Determine architecture", err)
+					return err
 				} else if info.IsDir() == false && strings.Contains(info.Name(), ".so.") {
 					arch, err := helpers.GetElfArchitecture(path)
-					helpers.PrintError("Determine architecture", err)
-					if helpers.SliceContains(archs, arch) == false {
+					if err != nil {
+						// we received an error when analyzing the arch
+						helpers.PrintError("Determine architecture", err)
+						return err
+					} else if helpers.SliceContains(archs, arch) == false {
 						log.Println("Architecture of", info.Name()+":", arch)
+						archs = helpers.AppendIfMissing(archs, arch)
+					} else {
+						// FIXME: we found some data, but still it was not a part of the
+						// known architectures
+						errArchNotKnown := errors.New("Could not detect a valid architecture")
+						return errArchNotKnown
 					}
-					archs = helpers.AppendIfMissing(archs, arch)
 				}
 				return nil
 			})

commit 11bfd62ebe1ca5058abc223601fb0065483b054f

fix: regression introduced by https://github.com/probonopd/go-appimage/commit/71d52986d56ab486f97f4e6a998a1d7495b47e3c
needed to predefine the variable before the if block
as scopes in go are also limited to the if-else-block only.

src/appimagetool/appimagetool.go
@@ -574,8 +574,14 @@ func GenerateAppImage(appdir string) {
 		}
 	}
 
-	if updateinformation != "" {
+	// declare an empty digest
+	// we will replace this digest with a sha256 signature if the appimage
+	// does not contain update information.
+	// if it does contain update information, we should first try to sign it
+	// with the PGP signature
+	digest := ""
 
+	if updateinformation != "" {
 		err = helpers.ValidateUpdateInformation(updateinformation)
 		if err != nil {
 			helpers.PrintError("VerifyUpdateInformation", err)
@@ -594,7 +600,7 @@ func GenerateAppImage(appdir string) {
 		// This is not part of the AppImageSpec yet, but in the future we will want to put this into the AppImageSpec:
 		// If an AppImage is not signed, it should have the SHA256 digest in the '.sha256_sig' section; this might
 		// eventually remove the need for an extra '.digest_md5' section and hence simplify the format
-		digest := helpers.CalculateSHA256Digest(target)
+		digest = helpers.CalculateSHA256Digest(target)
 		err = helpers.EmbedStringInSegment(target, ".sha256_sig", digest)
 		if err != nil {
 			helpers.PrintError("EmbedStringInSegment", err)