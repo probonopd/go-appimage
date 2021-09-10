672d8d94c04ff681d60a0c38e14abc8e175bb587
Caleb Gardner <calebq23@gmail.com>
2021-09-10 04:52:42 -0500

    Some more debugging outputs

--- scripts/build.sh
+++ scripts/build.sh
@@ -136,8 +136,8 @@ NoDisplay=true
 EOF
   fi
   chmod +x $BUILDDIR/$PROG-$ARCH.AppDir/usr/bin/*
-  ls $BUILDDIR/appimagetool-$ARCH.AppDir/usr/bin/
-  $BUILDDIR/appimagetool-$ARCH.AppDir/usr/bin/appimagetool $BUILDDIR/$PROG-$ARCH.AppDir
+  file $BUILDDIR/appimagetool-$ARCH.AppDir/usr/bin/appimagetool
+  # $BUILDDIR/appimagetool-$ARCH.AppDir/usr/bin/appimagetool $BUILDDIR/$PROG-$ARCH.AppDir
 }
 #############################################################
 # Setup environment
