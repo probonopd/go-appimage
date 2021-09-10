658eca5de2b1e42e4b8a253f5a79d8739155c7f0
Caleb Gardner <calebq23@gmail.com>
2021-09-10 06:40:12 -0500

    Sudo not needed

--- scripts/build.sh
+++ scripts/build.sh
@@ -186,17 +186,17 @@ fi
 
 # Install dependencies if needed
 if [ $GITHUB_ACTIONS ]; then
-  sudo apt-get update
-  sudo apt-get install --yes wget file gcc
+  apt-get update
+  apt-get install --yes wget file gcc
   for arch in ${BUILDARCH[@]}; do
     if [ $arch == arm ]; then
-      sudo dpkg --add-architecture armhf
-      sudo apt-get update
-      sudo apt-get install libc6:armhf zlib1g:armhf zlib1g-dev:armhf libfuse2:armhf libc6-armel:armhf
+      dpkg --add-architecture armhf
+      apt-get update
+      apt-get install libc6:armhf zlib1g:armhf zlib1g-dev:armhf libfuse2:armhf libc6-armel:armhf
     elif [ $arch == 386 ]; then
-      sudo dpkg --add-architecture i386
-      sudo apt-get update
-      sudo apt-get install libc6:i386 zlib1g:i386 libfuse2:i386
+      dpkg --add-architecture i386
+      apt-get update
+      apt-get install libc6:i386 zlib1g:i386 libfuse2:i386
     fi
   done
 fi
