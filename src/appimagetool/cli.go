eeb902e1acc9b94793d6d8630b378abed197c741
probonopd <probonopd@users.noreply.github.com>
2022-05-08 15:33:04 +0000

    Use helpers.CheckForNeededTools(tools)
    https://github.com/probonopd/go-appimage/issues/208

--- src/appimagetool/cli.go
+++ src/appimagetool/cli.go
@@ -158,8 +158,11 @@ func bootstrapAppImageBuild(c *cli.Context) error {
 	tools := []string{"file", "mksquashfs", "desktop-file-validate", "uploadtool", "patchelf", "desktop-file-validate", "patchelf"} // "sh", "strings", "grep" no longer needed?; "curl" is needed for uploading only, "glib-compile-schemas" is needed in some cases only
 	// curl is needed by uploadtool; TODO: Replace uploadtool with native Go code
 	// "sh", "strings", "grep" are needed by appdirtool to parse qt_prfxpath; TODO: Replace with native Go code
-	helpers.CheckIfAllToolsArePresent(tools)
-
+	err := helpers.CheckForNeededTools(tools)
+	if err != nil {
+		os.Exit(1)
+	}
+	
 	// Check whether we have a sufficient version of mksquashfs for -offset
 	if helpers.CheckIfSquashfsVersionSufficient("mksquashfs") == false {
 		os.Exit(1)
