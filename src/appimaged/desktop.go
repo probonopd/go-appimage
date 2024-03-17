ee8df1ac0e5c3c323a9c04dbd7befccdfcb95da0
Etienne Blondelle <etienneblondelle@gmail.com>
2024-03-17 16:07:38 +0100

    refactor(appimaged): use filepath.Join to avoid double slashes

--- src/appimaged/desktop.go
+++ src/appimaged/desktop.go
@@ -133,7 +133,7 @@ func writeDesktopFile(ai AppImage) error {
 			cfg.Section("Desktop Action Trash").Key("Exec").SetValue("kioclient move \"" + ai.Path + "\" trash:/")
 		} else {
 			// Provide a fallback shell command to prevent parser errors on other desktops
-			cfg.Section("Desktop Action Trash").Key("Exec").SetValue("mv \"" + ai.Path + "\" " + os.UserHomeDir() + "/.local/share/Trash/")
+			cfg.Section("Desktop Action Trash").Key("Exec").SetValue("mv \"" + ai.Path + "\" \"" + filepath.Join(os.UserHomeDir() + "/.local/share/Trash/") + "\"")
 		}
 
 		// Add OpenPortableHome action
